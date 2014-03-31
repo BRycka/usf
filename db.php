a0482fb4a7ab250e0cff514f127f58ff13871bc9
Ricardas <BRycka121@gmail.com>
2014-03-31 12:22:09 +0300

    Pridėtas vartotojų sąrašas

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ricblt
+ * Date: 31/03/14
+ * Time: 10:18
+ */
+$mysqli = new mysqli("localhost", "root", "", "USF");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+/* change character set to utf8 */
+$mysqli->set_charset("utf8");
+/* Print current character set */
+//$charset = $mysqli->character_set_name();
+//printf ("Current character set is %s\n", $charset);
---
+++ update_employee.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ricblt
+ * Date: 31/03/14
+ * Time: 11:28
+ */
+require('db.php');
+require('form.php');
+form('Update');
